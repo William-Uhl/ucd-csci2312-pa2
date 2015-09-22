d9b8963f08c181691dbebc8c9e28a41a36123b63
William-Uhl <William.uhl@ucdenver.edu>
2015-09-21 22:10:22 -0600

    Cluster header is done

---
+++ Cluster.h
@@ -0,0 +1,66 @@
+//
+// Created by Tres on 9/15/2015.
+//
+
+#ifndef UHL_PA2_CLUSTER_H
+#define UHL_PA2_CLUSTER_H
+
+
+#include "Point.h"
+
+namespace Clustering {
+
+    typedef Point *PointPtr;
+    typedef struct LNode *LNodePtr;
+
+    struct LNode {
+        PointPtr p;
+        LNodePtr next;
+    };
+
+    class Cluster {
+        int size;
+        LNodePtr points;
+
+    public:
+        //default constructor
+        Cluster() : size(0), points(nullptr) {};
+
+        // The big three: cpy ctor, overloaded operator=, dtor
+        Cluster(const Cluster &);
+        Cluster &operator=(const Cluster &);
+        ~Cluster();
+
+        // Set functions: They allow calling c1.add(c2.remove(p));
+        void add(const PointPtr &);
+        const PointPtr &remove(const PointPtr &);
+
+        // Overloaded operators
+
+        // IO
+        friend std::ostream &operator<<(std::ostream &, const Cluster &);
+        friend std::istream &operator>>(std::istream &, Cluster &);
+
+        // Set-preserving operators (do not duplicate points in the space)
+        // - Friends
+        friend bool operator==(const Cluster &lhs, const Cluster &rhs);
+
+        // - Members
+        Cluster &operator+=(const Cluster &rhs); // union
+        Cluster &operator-=(const Cluster &rhs); // (asymmetric) difference
+
+        Cluster &operator+=(const Point &rhs); // add point
+        Cluster &operator-=(const Point &rhs); // remove point
+
+        // Set-destructive operators (duplicate points in the space)
+        // - Friends
+        friend const Cluster operator+(const Cluster &lhs, const Cluster &rhs);
+        friend const Cluster operator-(const Cluster &lhs, const Cluster &rhs);
+
+        friend const Cluster operator+(const Cluster &lhs, const PointPtr &rhs);
+        friend const Cluster operator-(const Cluster &lhs, const PointPtr &rhs);
+
+    };
+
+}
+#endif //UHL_PA2_CLUSTER_H
