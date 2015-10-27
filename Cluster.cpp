d7b0ba62d7030c7cb053e6e3e6b3627e6a3f33b9
William-Uhl <William.uhl@ucdenver.edu>
2015-10-26 23:54:14 -0600

    just need Kmeans

--- Cluster.cpp
+++ Cluster.cpp
@@ -2,14 +2,28 @@
 // Created by William Uhl on 9/15/2015.
 //
 
+
 #include "Cluster.h"
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <bits/stl_algo.h>
+
+using std::string;
 
 namespace Clustering {
+
+    //For PA3
+    const char Cluster::POINT_CLUSTER_ID_DELIM = ':';
+
+//    unsigned int Cluster::__id = 1;
 
 //Copy constructor works
     Cluster::Cluster(const Cluster &inputCluster) {
         size = inputCluster.size;
-
+        __centroid = inputCluster.__centroid;
+        __validCen = true;
+        idGen();
         if (inputCluster.points == nullptr)
             points = nullptr;
         else {
@@ -39,6 +53,9 @@ Cluster &Cluster::operator=(const Cluster &inputCluster) {
             return *this;
 
         size = inputCluster.size;
+        __centroid = inputCluster.__centroid;
+        __validCen = true;
+        idGen();
 
         if (inputCluster.points == nullptr)
             points = nullptr;
@@ -157,24 +174,39 @@ std::ostream &operator<<(std::ostream &os, const Cluster &cluster) {
             return os;
         }
 
+
         LNodePtr current = cluster.points;
         //int counter =1;
         for( ; current != nullptr; current = current->next) {
            // os << "Point " << counter++ << std::endl;
-            os << *(current->p) << std::endl;
-
+            os << *(current->p) <<
+            cluster.getID() << std::endl;
 
         }
+
         return os;
 
 
 }
 
-    //Will do this for the next PA assignment
+
 //overload operator>>
-//std::istream &operator>>(std::istream &istream, Cluster &cluster) {
-   // return <#initializer#>;
-//}
+std::istream &operator>>(std::istream & inStream, Cluster &cluster) {
+
+        std::string input;
+        static const char DELIMINATOR = ',';
+        while (getline(inStream,input)) {
+            int num = std::count(input.begin(),input.end(),DELIMINATOR) + 1;
+
+            cluster.setDim(num);
+            PointPtr p;
+            p = new Point(num);
+
+            std::stringstream lineStream(input);
+            lineStream >> *p;
+            cluster.add(p);
+        }
+}
 
 //overload operator== works
     bool operator==(const Cluster &lhs, const Cluster &rhs) {
@@ -201,13 +233,13 @@ std::ostream &operator<<(std::ostream &os, const Cluster &cluster) {
 
     }
 
-//overload operator+=    DOES NOT WORK
+//overload operator+=    Maybe WORK
 Cluster &Cluster::operator+=(const Cluster &rhs) {
 
 
         LNodePtr left = this->points;
         LNodePtr right = rhs.points;
-        Cluster c(rhs);
+
         bool same = false;
 
             while (left != nullptr) {
@@ -221,24 +253,24 @@ Cluster &Cluster::operator+=(const Cluster &rhs) {
                     right = right->next;
                 }
                 if (same == false) {
-                    c.add(left->p);
-                    c.size++;
+                    this->add(left->p);
+                    this->size++;
                 }
                 left = left->next;
             }
-        return c;
+        return *this;
 }
 
-//overload operator-=    DOES NOT WORK
+//overload operator-=    Maybe WORK
 Cluster &Cluster::operator-=(const Cluster &rhs) {
 
         LNodePtr left = this->points;
         LNodePtr right = rhs.points;
-        Cluster c = Cluster(*this);
+
         bool same = false;
 
         if(this->size == 0) {
-            return c;
+            return *this;
         }
 
         else{
@@ -254,14 +286,14 @@ Cluster &Cluster::operator-=(const Cluster &rhs) {
 
                 }
                 if (same == true) {
-                    c.remove(left->p);
-                    c.size--;
+                    this->remove(left->p);
+                    this->size--;
                 }
                 left = left->next;
             }
         }
 
-        return c;
+        return *this;
 }
 
 //overload operator+=  works
@@ -308,6 +340,10 @@ Cluster &Cluster::operator-=(const Point &rhs) {
 //overload operator+ with two clusters  CLOSE BUT DOES NOT WORK
 const Cluster operator+(const Cluster &lhs, const Cluster &rhs) {
 
+//        Cluster c(lhs);
+//        c+= rhs;
+//        return c;
+
         LNodePtr left = lhs.points;
         LNodePtr right = rhs.points;
         Cluster c(rhs);
@@ -334,37 +370,41 @@ const Cluster operator+(const Cluster &lhs, const Cluster &rhs) {
 
 //overload operator- with two clusters      CLOSE BUT DOES NOT WORK
 const Cluster operator-(const Cluster &lhs, const Cluster &rhs) {
-        LNodePtr left = lhs.points;
-        LNodePtr right = rhs.points;
-        Cluster c = Cluster();
-        bool same = false;
 
-        if(lhs.size == 0) {
-            return c;
-        }
-
-        else{
-
-            while (left != nullptr) {
-
-                while (right != nullptr) {
-                    if (left->p == right->p) {
-
-                        same = true;
-                    }
-                    right = right->next;
-
-                }
-                if (same == false) {
-                    c.add(left->p);
-                    c.size++;
-                }
-                left = left->next;
-            }
-        }
-
-
+        Cluster c(lhs);
+        c-=rhs;
         return c;
+//        LNodePtr left = lhs.points;
+//        LNodePtr right = rhs.points;
+//        Cluster c = Cluster();
+//        bool same = false;
+//
+//        if(lhs.size == 0) {
+//            return c;
+//        }
+//
+//        else{
+//
+//            while (left != nullptr) {
+//
+//                while (right != nullptr) {
+//                    if (left->p == right->p) {
+//
+//                        same = true;
+//                    }
+//                    right = right->next;
+//
+//                }
+//                if (same == false) {
+//                    c.add(left->p);
+//                    c.size++;
+//                }
+//                left = left->next;
+//            }
+//        }
+//
+//
+//        return c;
 }
 
 //overload operator+ with a cluster and a point WORKS
@@ -434,6 +474,132 @@ const Cluster operator-(const Cluster &lhs, const Cluster &rhs) {
 
         p = ptr;
         next = nodePtr;
+
+    }
+
+    void Cluster::setCentroid(const Point &point) {
+        PointPtr cent = new Point(point);
+        __centroid = cent;
+        __validCen = true;
+    }
+
+    const PointPtr Cluster::getCentroid() {
+        return __centroid;
+    }
+
+    void Cluster::computeCentroid() {
+        Point cent(__dims);
+        LNodePtr curr = points;
+        if (curr == NULL)
+            __centroid = NULL;
+        for(int i=0;i <= size; i++){
+            cent += *(curr->p)/size;
+            curr = curr->next;
+        }
+        PointPtr c = new Point(cent);
+        __centroid = c;
+        __validCen = true;
+    }
+
+    void Cluster::idGen() {
+        static unsigned int id = 1;
+        __id = id++;
+
+    }
+
+    void Cluster::invalidCentroid() {
+        __validCen = false;
+    }
+
+    void Cluster::setDim(const int dim) {
+        __dims = dim;
+
+    }
+
+    void Cluster::pickPoints(int k, PointPtr *pointArray) {
+
+        int size= this->size;
+        int num = size/k;
+        LNodePtr curr = this->points;
+
+        for(int i = 0;i<k;i++){
+            pointArray[i] = curr->p;
+            for(int j = 0; j<num; j++)
+                curr = curr->next;
+
+        }
+
+    }
+
+    double Cluster::intraClusterDistance() const {
+        LNodePtr curr = this->points;
+        double dist = 0;
+        while(curr != NULL){
+            LNodePtr other = this->points;
+            while (other != NULL) {
+                dist = (dist + curr->p->distanceTo(*(other->p))) / 2;
+                other = other->next;
+            }
+            curr = curr->next;
+        }
+
+    return dist;
+    }
+
+    double Cluster::interClusterDistance(const Cluster &c1, const Cluster &c2) {
+        LNodePtr curr = c1.points;
+        double dist = 0;
+        while(curr != NULL){
+            LNodePtr other = c2.points;
+            while (other != NULL) {
+                dist = (dist + curr->p->distanceTo(*(other->p))) / 2;
+                other = other->next;
+            }
+            curr = curr->next;
+        }
+
+        return dist;
+    }
+
+    int Cluster::getClusterEdges() {
+        return((this->size)*((this->size) - 1)/2);
+
+    }
+
+//    double Clustering::interClusterEdges(const Cluster &c1, const Cluster &c2) {
+//        double size1 = c1.size;
+//        double size2 = c2.size;
+//        double edges = (size1 *(size2 - 1))/2;
+//        return edges;
+//
+//    }
+
+//    double Cluster::Kmeans::computeClusteringScore(int k, Cluster *cluster) {
+//        double dIN = 0;
+//        double dOUT = 0;
+//        int pIN = 0;
+//        int pOUT = 0;
+//        for(int i = 0;i<k; i++)
+//            dIN = dIN + cluster[i].intraClusterDistance();
+//
+//        for (int i = 0; i<k-1; i++){
+//            for(int j = i; j<k; j++)
+//                dOUT = dOUT + cluster->interClusterDistance(cluster[i],cluster[j]);
+//        }
+//
+//        for(int i =0;i<k;i++)
+//            pIN = pIN + cluster[i].getClusterEdges();
+//
+//        for(int i = 0;i<k-1; i++){
+//            for(int j=i+1;j<k; j++)
+//                pOUT = pOUT + cluster->interClusterEdges(cluster[i],cluster[j]);
+//
+//        }
+//        double betaCV = (dIN/pIN)/(dOUT/pOUT);
+//        return betaCV;
+//    }
+
+    void Cluster::Kmeans::runKmeans() {
 
     }
 }
