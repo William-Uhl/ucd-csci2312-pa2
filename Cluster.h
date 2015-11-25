8339bf1d6234e039e0deb113b3a7cd376a634c1a
William-Uhl <William.uhl@ucdenver.edu>
2015-11-24 23:33:05 -0700

    needs more testing

--- Cluster.h
+++ Cluster.h
@@ -26,18 +26,23 @@ namespace Clustering {
     public:
 
         //hashmap function
-        int genHash( int, int) const;
+        int genHash(int, int) const;
 
         //New centroid function
         void setCentroid(const T &);
-        bool centroidValid() { return __validCen;}
+
+        bool centroidValid() { return __validCen; }
+
         const T getCentroid();
+
         void computeCentroid();
+
         void invalidCentroid();
 
         //id for cluster
         void idGen();
-        unsigned int getID() const {return __id;}
+
+        unsigned int getID() const { return __id; }
 
 
         // for use with operator<<
@@ -45,98 +50,93 @@ namespace Clustering {
 
 
         //New cluster functionality
-        void setDim(const int dim);
+        void setDim(unsigned int inputdim) { __dims = inputdim; };
+
         void pickPoints(int k, std::vector<T> &);
-        int getSize() {return size;}
+
+        int getSize() { return size; }
+
         double intraClusterDistance() const;
-        double interClusterDistance(const Cluster<T,dim> &c1, const Cluster<T,dim> &c2);
+
+        double interClusterDistance(const Cluster<T, dim> &c1, const Cluster<T, dim> &c2);
+
         int getClusterEdges();
 
-        template <typename S, int _dim>
-        friend double interClusterEdges(const Cluster<S,dim> &c1, const Cluster<S,dim> &c2);
+        template<typename S, int _dim>
+        friend double interClusterEdges(const Cluster<S, dim> &c1, const Cluster<S, dim> &c2);
 
         //default constructor
-        Cluster() : size(0), points(nullptr), __centroid(0) {};
+        Cluster() : size(0), points(nullptr), __centroid(0) { };
 
         // The big three: cpy ctor, overloaded operator=, dtor
         Cluster(const Cluster &);
+
         Cluster &operator=(const Cluster &);
+
         ~Cluster();
 
         //remove and add a point
         void add(const T &);
+
         const T &remove(const T &);
 
         // Overloaded operators
 
         // IO
-        template <typename S, int _dim>
-        friend std::ostream &operator<<(std::ostream &, const Cluster<S,_dim> &);
+        template<typename S, int _dim>
+        friend std::ostream &operator<<(std::ostream &, const Cluster<S, _dim> &);
 
-        template <typename S, int _dim>
-        friend std::istream &operator>>(std::istream &, Cluster<S,_dim> &);
+        template<typename S, int _dim>
+        friend std::istream &operator>>(std::istream &, Cluster<S, _dim> &);
 
         // Set-preserving operators (do not duplicate points in the space)
         // - Friends
-        template <typename S, int _dim>
-        friend bool operator==(const Cluster<S,_dim> &lhs, const Cluster<T,_dim> &rhs);
+        template<typename S, int _dim>
+        friend bool operator==(const Cluster<S, _dim> &lhs, const Cluster<T, _dim> &rhs);
 
         // - Members
-        Cluster<T,dim> &operator+=(const Cluster<T,dim> &rhs); // union
-        Cluster<T,dim> &operator-=(const Cluster<T,dim> &rhs); // (asymmetric) difference
+        Cluster<T, dim> &operator+=(const Cluster<T, dim> &rhs); // union
+        Cluster<T, dim> &operator-=(const Cluster<T, dim> &rhs); // (asymmetric) difference
 
-        Cluster<T,dim> &operator+=(const T &rhs);  // add point
-        Cluster<T,dim> &operator-=(const T &rhs); // remove point
+        Cluster<T, dim> &operator+=(const T &rhs);  // add point
+        Cluster<T, dim> &operator-=(const T &rhs); // remove point
 
         // Set-destructive operators (duplicate points in the space)
         // - Friends
-        template <typename S, int _dim>
-        friend const Cluster<S,_dim> operator+(const Cluster<S,_dim> &lhs, const Cluster<S,_dim> &rhs);
+        template<typename S, int _dim>
+        friend const Cluster<S, _dim> operator+(const Cluster<S, _dim> &lhs, const Cluster<S, _dim> &rhs);
 
-        template <typename S, int _dim>
-        friend const Cluster<S,_dim> operator-(const Cluster<S,_dim> &lhs, const Cluster<S,_dim> &rhs);
+        template<typename S, int _dim>
+        friend const Cluster<S, _dim> operator-(const Cluster<S, _dim> &lhs, const Cluster<S, _dim> &rhs);
 
-        template <typename S, int _dim>
-        friend const Cluster<S,_dim> operator+(const Cluster<S,_dim> &lhs, const S &rhs);
+        template<typename S, int _dim>
+        friend const Cluster<S, _dim> operator+(const Cluster<S, _dim> &lhs, const S &rhs);
 
-        template <typename S, int _dim>
-        friend const Cluster<S,_dim> operator-(const Cluster<S,_dim> &lhs, const S &rhs);
+        template<typename S, int _dim>
+        friend const Cluster<S, _dim> operator-(const Cluster<S, _dim> &lhs, const S &rhs);
 
 
         //Inner class Move
-        class Move{
-                private:
-                    T ptr;
-                    Cluster<T,dim> *from, *to;
-                public:
-                    Move(T &ptr,Cluster<T,dim> *from, Cluster<T,dim> *to) :
-                    ptr(ptr), from(from), to(to)
-                    {}
-                    void perform() {to->add(from->remove(ptr));};
+        class Move {
+        private:
+            T ptr;
+            Cluster<T, dim> *from, *to;
+        public:
+            Move(T &ptr, Cluster<T, dim> *from, Cluster<T, dim> *to) :
+                    ptr(ptr), from(from), to(to) { }
+
+            void perform() { to->add(from->remove(ptr)); };
 
 
         };
 
-        T &operator[](int i){
+        T &operator[](int i) {
             T curr = this->points;
-            for(int j = 0; j<i; j++){
+            for (int j = 0; j < i; j++) {
                 if (curr->next != NULL)
                     curr = curr->next;
             }
         }
-        //Kmeans
-        class Kmeans{
-        private:
-
-
-        public:
-            static constexpr double SCORE_DIFF_THRESHOLD = .001;
-            double computeClusteringScore(int k, Cluster<T,dim> *cluster);
-            void runKmeans(int k,int dim);
-
-
-        };
-
     };
 
 }
