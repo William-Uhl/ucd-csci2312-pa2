5bf5b18d969a373101d9a336cc0ac51c894b7bd7
William-Uhl <William.uhl@ucdenver.edu>
2015-11-24 23:32:17 -0700

    Needs more testing

--- Cluster.cpp
+++ Cluster.cpp
@@ -14,9 +14,6 @@ using std::string;
 
 namespace Clustering {
 
-    //For PA3
-    const char Cluster::POINT_CLUSTER_ID_DELIM = ':';
-
 //    unsigned int Cluster::__id = 1;
 
 //Copy constructor works
@@ -70,7 +67,7 @@ Cluster<T,dim> &Cluster<T,dim>::operator=(const Cluster<T,dim> &inputCluster) {
 
 //destructor
     template <typename T, int dim>
-    Cluster::~Cluster() {
+    Cluster<T,dim>::~Cluster() {
         points.clear();
     }
 
@@ -381,11 +378,6 @@ const Cluster<S,dim> operator-(const Cluster<S,dim> &lhs, const Cluster<S,dim> &
         __validCen = false;
     }
 
-    template <typename T, int dim>
-    void Cluster<T,dim>::setDim(const int dim) {
-        __dims = dim;
-
-    }
 
     template <typename T, int dim>
     void Cluster<T,dim>::pickPoints(int k, std::vector<T> &pointArray) {
@@ -474,108 +466,7 @@ const Cluster<S,dim> operator-(const Cluster<S,dim> &lhs, const Cluster<S,dim> &
     }
 
     template <typename T, int dim>
-    double Cluster<T,dim>::Kmeans::computeClusteringScore(int k, Cluster<T,dim> *cluster) {
-        double dIN = 0;
-        double dOUT = 0;
-        double pIN = 0;
-        double pOUT = 0;
-        for (int i = 0; i < k; i++)
-            dIN = dIN + cluster[i].intraClusterDistance();
-
-        for (int i = 0; i < k - 1; i++) {
-            for (int j = i; j < k; j++) {
-                Cluster<T,dim> c = cluster[i];
-                Cluster<T,dim> c2 = cluster[j];
-
-                dOUT = dOUT + cluster->interClusterDistance(c,c2);
-            }
-        }
-
-
-        for(int i =0;i<k;i++)
-            pIN = pIN + cluster[i].getClusterEdges();
-
-        for(int i = 0;i<k-1; i++){
-            for(int j=i+1;j<k; j++) {
-                Cluster<T,dim> c = cluster[i];
-                Cluster<T,dim> c2 = cluster[j];
-
-                pOUT = pOUT + interClusterEdges(c,c2);
-            }
-        }
-        double betaCV = (dIN/pIN)/(dOUT/pOUT);
-        return betaCV;
-    }
-
-
-
-    void Cluster::Kmeans::runKmeans(int k, int dim) {
-
-        PointPtr centArray[k];
-        Cluster point_space;
-
-        std::ifstream csv("points.txt");
-        point_space.setDim(dim);
-        csv >> point_space;
-        point_space.pickPoints(k,centArray);
-        point_space.setCentroid(*centArray[k]);
-
-        Cluster *cArr = new Cluster[k];
-        cArr[k] = point_space;
-
-
-        for(int i = 0; i<k-1; i++){
-            Cluster *add = new Cluster;
-            add->setDim(dim);
-            add->setCentroid(*centArray[i]);
-            cArr[i] = *add;
-        }
-
-        double score = computeClusteringScore(k,cArr);
-        double prevScore;
-        double scoreDiff = SCORE_DIFF_THRESHOLD + 1;
-
-        while(scoreDiff > SCORE_DIFF_THRESHOLD){
-            //loop through clusters
-            for(int i =0; i < k; i++){
-                //loop through points
-                for(int j=0;j<cArr[i].getSize();j++){
-                    Cluster *move = NULL;
-                    //loop through centroids
-                    for(int l = 0;l < k; l++){
-                        PointPtr curr = cArr[i][j]->p;
-                        if (curr->distanceTo(cArr[i].getCentroid()) > curr->distanceTo(cArr[l].getCentroid())){
-
-                            move = &cArr[l];
-                        }
-
-                    }
-
-                     Move(cArr[i][j]->p, &cArr[i], move);
-
-
-                }
-            }
-            for(int i = 0;i< k; i++){
-                if(!(cArr[i].centroidValid()))
-                    cArr[i].computeCentroid();
-            }
-            prevScore = score;
-
-            score = computeClusteringScore(k,cArr);
-            scoreDiff = fabs(score-prevScore);
-        }
-
-
-        std::ofstream out("output.txt");
-        for(int j = 0;j<k;j++)
-            out << cArr[j];
-        out.close();
-
-    }
-
-
-    int Cluster::genHash(int i, int i1) const {
+    int Cluster<T,dim>::genHash(int i, int i1) const {
         //cantors functions
         if(i > i1){
             int hash = .5*((i + i1)*(i+i1+1))+i1;
